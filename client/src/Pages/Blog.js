f15886f07d39e61021317997bc5c495b954a4f06
NerdyVisky <vishvesh106@gmail.com>
2022-02-21 22:28:21 +0530

    added home page responsivness

--- client/src/Pages/Blog.js
+++ client/src/Pages/Blog.js
@@ -5,15 +5,23 @@ import nftImgURL from '../assets/img/nft.png';
 const Blog = () => {
     const API_URL = 'http://localhost:8000/api/blogs';
     const [blogs, setBlogs] = useState([]);
+    const [blogsLoaded, setBlogsLoaded] = useState(false);
     const blog = blogs[2];
     useEffect(() => {
         fetch(API_URL)
         .then(res => res.json())
         .then(data => setBlogs(data))
+        .then(() => setBlogsLoaded(true))
         .catch(err => console.log(err)); 
     }, [])
+    let blogJSX = null;
+    if(blogsLoaded){
+        blogJSX = 
+        <div>{blogs[0].title}</div>
+    }
     return ( 
     <div id="blog">
+       {blogJSX}
     </div> 
     );
 }
